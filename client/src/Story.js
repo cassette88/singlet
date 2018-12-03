8c6a8e6d839ccbec34c06f4c8e79e6ce4f595e8a
Dave <Dave@Daves-Mac-mini.local>
2018-12-02 22:11:10 -0500

    background image

--- client/src/Story.js
+++ client/src/Story.js
@@ -1,14 +1,16 @@
 import React from  'react';
+import ball from './img/tb.jpg';
 
 
 
 
 const divStyle ={
- // backgroundImage: `url(${ball})`,
+  backgroundImage: `url(${ball})`,
   //backgroundImage: 'url("https://images.unsplash.com/photo-1535480436112-07697fcbcbea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=31f51839cb4319385e043204acd13491&auto=format&fit=crop&w=400&q=60")',
  // backgroundSize: 'contain',
   backgroundPosition: 'center',
   backgroundSize: 'cover',
+  
   
 };
 
@@ -61,9 +63,8 @@ amp = quote.replace(/&amp;/g, "and");
 render(){
  return (   
    
-   
-     
-      <div className="col-lg-4 col-md-6 col-sm-6" align="center" style={divStyle}>
+  
+  <div className="col-lg-4 col-md-6 col-sm-6" align="center" style={divStyle}>
      <div className="card-body">
     {/* <img className="card-img-top" src={require('./ball.png')} alt="tennis ball" /> */}
 
