07844f82c6055a7339d2fb73764eb761e901ff08
Dave <dave@Daves-iMac.local>
2018-12-03 13:20:35 -0500

    heroku css test

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
