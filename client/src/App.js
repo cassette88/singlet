501d09726c5980e517a14dd7273cc1bdb45e5d18
Dave <dave@Daves-iMac.local>
2018-12-05 23:00:35 -0500

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Stories from './Stories';
+import FooterPage from './FooterPage';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Stories />
+        <FooterPage />
       </div>
     );
   }
---
+++ client/src/FooterPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Col, Container, Row, Footer } from "mdbreact";
+
+class FooterPage extends React.Component {
+render() {
+return (
+<Footer color="blue-grey darken-3" className="font-small pt-4 mt-4">
+  <Container fluid className="text-center text-md-left">
+    <Row>
+      <Col md="6">
+      <h5 className="title">College Singlet</h5>
+      <p>
+      </p>
+      </Col>
+      <Col md="6">
+      <p>* All photographs by:</p>
+      <a href="https://www.flickr.com/photos/140049134@N06/albums"><h5 className="title">Cam Kramer</h5></a>
+      <ul>
+        <li className="list-unstyled">
+          <a href="https://www.flickr.com/photos/chrishunkeler/"><h5>Chris Hunkeler</h5></a>
+        </li>
+      
+      </ul>
+      </Col>
+    </Row>
+  </Container>
+  <div className="footer-copyright text-center py-3">
+    {/* <Container fluid>
+      &copy; {new Date().getFullYear()} Copyright:{" "}
+      <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+    </Container> */}
+  </div>
+</Footer>
+);
+}
+}
+
+export default FooterPage;
